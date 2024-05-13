c5a4965358e60744666a1c3731e1ff7ec8bccf09
Thomas Clark <thomas.clark@bigtincan.com>
2024-05-13 12:52:35 +1000

    init eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tsEslint from "typescript-eslint";
+
+/**
+ * @type {import("eslint").Linter.FlatConfig[]}
+ */
+export default [
+	{ languageOptions: { globals: globals.node }, files: ["**/*.ts"] },
+	pluginJs.configs.recommended,
+	...tsEslint.configs.recommended,
+];
