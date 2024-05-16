32c8251c41bd7f694267d2dd7c3c56c9ea3bdd18
Thomas Clark <thomas.clark@bigtincan.com>
2024-05-16 16:15:19 +1000

    removed problematic content just to get a working deploy up

--- packages/docs/src/pages/config-schema.json.ts
+++ packages/docs/src/pages/config-schema.json.ts
@@ -1,9 +1,9 @@
 import type { APIRoute } from "astro";
-import zodToJsonSchema from "zod-to-json-schema";
-import { workspaceEnvConfigInputSchema } from "workspace-env";
+// import zodToJsonSchema from "zod-to-json-schema";
+// import { workspaceEnvConfigInputSchema } from "workspace-env";
 
 export const GET: APIRoute = async () =>
-  new Response(JSON.stringify(zodToJsonSchema(workspaceEnvConfigInputSchema)), {
+  new Response(JSON.stringify({ placeholder: true }), {
     headers: {
       "content-type": "application/json",
     },
