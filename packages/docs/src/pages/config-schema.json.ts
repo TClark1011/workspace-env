e45bff80163e95c18639d8c013c738267fc0d0e5
Thomas Clark <thomas.clark@bigtincan.com>
2024-05-21 15:18:02 +1000

    allow "$schema" field in json config schema

--- packages/docs/src/pages/config-schema.json.ts
+++ packages/docs/src/pages/config-schema.json.ts
@@ -1,9 +1,14 @@
 import type { APIRoute } from "astro";
 import zodToJsonSchema from "zod-to-json-schema";
 import { workspaceEnvConfigInputSchema } from "workspace-env";
+import { z } from "zod";
+
+const finalConfigInputSchema = workspaceEnvConfigInputSchema.extend({
+  $schema: z.string().optional(),
+});
 
 export const GET: APIRoute = async () =>
-  new Response(JSON.stringify(zodToJsonSchema(workspaceEnvConfigInputSchema)), {
+  new Response(JSON.stringify(zodToJsonSchema(finalConfigInputSchema)), {
     headers: {
       "content-type": "application/json",
     },
